152eb3f4ab98c620d87dd290ce5034fb1d732dcf
Alex Sherman <alexsherman257@gmail.com>
2023-11-20 15:52:10 +1100

    Assesment of feature importance in prediction model

--- modelV2.py
+++ modelV2.py
@@ -5,9 +5,7 @@ Created on Mon Nov 20 14:34:49 2023
 @author: alexs
 """
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 
 
@@ -26,7 +24,7 @@ print(model.fit().summary())
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 
@@ -60,9 +58,26 @@ grid_search = GridSearchCV(estimator=rf, param_grid=param_grid,
                            cv=3, n_jobs=-1, verbose=2, scoring='neg_mean_absolute_error')
 
 grid_search.fit(X_train, y_train)
+best_rf_model = grid_search.best_estimator_
 
 # Test ensembles
-tpred_rf = grid_search.best_estimator_.predict(X_test)
+tpred_rf = best_rf_model.predict(X_test)
 print(mean_absolute_error(y_test, tpred_rf))
 
+# Analysing the importance of each feature
+
+importances = best_rf_model.feature_importances_
+feature_names = X.columns
+feature_importances = pd.Series(importances, index=feature_names).sort_values(ascending=False)
+print(feature_importances)
+
+
+import shap
+
+# Calculate SHAP values
+explainer = shap.TreeExplainer(best_rf_model)
+shap_values = explainer.shap_values(X_train)
+
+# Plot SHAP values for the first few instances
+shap.summary_plot(shap_values, X_train, plot_type="bar")
 
